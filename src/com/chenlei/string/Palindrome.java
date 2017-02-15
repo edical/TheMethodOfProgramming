de9f88c8f3ef4b9e39242c17479bfd0c4db50f58
chen.lei <chen.lei@puscene.com>
2017-02-15 20:43:02 +0800

    回文处理

--- src/com/chenlei/string/Palindrome.java
+++ src/com/chenlei/string/Palindrome.java
@@ -9,7 +9,7 @@ import com.chenlei.util.StringUtils;
  */
 public class Palindrome {
 
-    public static boolean isPalindrome(String str) {
+    private static boolean isPalindrome(String str) {
         if(StringUtils.isBlank(str)) {
             return false;
         }
@@ -24,9 +24,29 @@ public class Palindrome {
         return true;
     }
 
+    private static boolean isPalindromeByMiddle(String str) {
+        int before = str.length() / 2 - 1, after;
+        if(str.length() % 2 == 0) {
+            after = str.length() / 2;
+        } else {
+            after = str.length() / 2 + 1;
+        }
+        while(before >= 0) {
+            if(str.charAt(before) != str.charAt(after)) {
+                return false;
+            }
+            before--;
+            after++;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println(isPalindrome("issi"));
         System.out.println(isPalindrome("abcba"));
         System.out.println(isPalindrome("abcea"));
+        System.out.println(isPalindromeByMiddle("issi"));
+        System.out.println(isPalindromeByMiddle("abcba"));
+        System.out.println(isPalindromeByMiddle("abcea"));
     }
 }
